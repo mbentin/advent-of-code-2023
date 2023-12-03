267cf817e704c26eac9463634aed786e16ae22ed
Maxime Bentin <maxime.bentin@gmail.com>
2023-12-03 14:34:31 +0100

    Day03 little cleanup

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -10,37 +10,38 @@ fun IntRange.isAdjacent(range: IntRange): Boolean {
     val extendedRange = IntRange(range.first - 1, range.last + 1)
     return this.first <= extendedRange.last && this.last >= extendedRange.first
 }
-fun main() {
 
+fun Sequence<MatchResult>.isAdjacent(range: IntRange): Boolean {
+    return this.map { range.isAdjacent(it.range) }
+        .fold(false) { acc, bool -> acc || bool }
+}
+
+fun main() {
+    val numbersR = """(\d+)""".toRegex()
 
     fun part1(input: List<String>): Int {
         var previousLine: SchemaLine? = null
         var topSymbols: Sequence<MatchResult>? = null
-        val numbersR = """(\d+)""".toRegex()
         val symbolsR = """([-+*#@/$&%=])""".toRegex()
         var sumOfPartNumbers = 0
+
         for ((index, string) in input.withIndex()) {
-                val numbersFound = numbersR.findAll(string)
-                val symbolsFound = symbolsR.findAll(string)
-            "NEWLINE".println()
-            // numbersFound.forEach { it.value.println() }
-            // symbolsFound.forEach { it.value.println() }
+            val numbersFound = numbersR.findAll(string)
+            val symbolsFound = symbolsR.findAll(string)
             var isAdjacentToTop = false
+
+            "NEWLINE".println()
+
             if (previousLine != null) {
                 // Calculate the previous line
-                for (number in previousLine!!.numbers) {
+                for (number in previousLine.numbers) {
                     if (topSymbols != null) {
-                        isAdjacentToTop = topSymbols!!
-                            .map { number.range.isAdjacent(it.range) }
-                            .fold(false) { acc, bool -> acc || bool }
+                        isAdjacentToTop = topSymbols.isAdjacent(number.range)
                     }
-                    val isAdjacentToPrevious = previousLine!!
+                    val isAdjacentToPrevious = previousLine
                         .symbols
-                        .map { number.range.isAdjacent(it.range) }
-                        .fold(false) {acc, bool -> acc || bool}
-                    val isAdjacentCurrent = symbolsFound
-                        .map { number.range.isAdjacent(it.range) }
-                        .fold(false) {acc, bool -> acc || bool}
+                        .isAdjacent(number.range)
+                    val isAdjacentCurrent = symbolsFound.isAdjacent(number.range)
                     "${number.value.toInt()} : $isAdjacentCurrent, $isAdjacentToPrevious".println()
                     if (isAdjacentToTop || isAdjacentToPrevious || isAdjacentCurrent) {
                         sumOfPartNumbers += number.value.toInt()
@@ -49,8 +50,8 @@ fun main() {
                 // Calculate the last line
                 if (index == string.lastIndex) {
                     for (number in numbersFound) {
-                        val isAdjacentToPrevious = previousLine!!.symbols.map { number.range.isAdjacent(it.range) }.fold(false) {acc, bool -> acc || bool}
-                        val isAdjacentCurrent = symbolsFound.map { number.range.isAdjacent(it.range) }.fold(false) {acc, bool -> acc || bool}
+                        val isAdjacentToPrevious = previousLine.symbols.isAdjacent(number.range)
+                        val isAdjacentCurrent = symbolsFound.isAdjacent(number.range)
                         "${number.value.toInt()} : $isAdjacentCurrent, $isAdjacentToPrevious".println()
                         if (isAdjacentToPrevious || isAdjacentCurrent) {
                             sumOfPartNumbers += number.value.toInt()
@@ -62,43 +63,36 @@ fun main() {
                 topSymbols = previousLine?.symbols
             }
             previousLine = SchemaLine(symbolsFound, numbersFound)
-            index.println()
         }
         return sumOfPartNumbers
     }
 
     fun part2(input: List<String>): Int {
-        val numbersR = """(\d+)""".toRegex()
         val starR = """([*])""".toRegex()
         var sumOfGearRatios = 0
         var previousLine: SchemaLine? = null
         var topNumbers: Sequence<MatchResult>? = null
 
-
         for ((index, string) in input.withIndex()) {
             val numbersFound = numbersR.findAll(string)
             val starFound = starR.findAll(string)
             var adjacentToTop: Sequence<Pair<Boolean, String>> = emptySequence()
-            "NEWE LINE: $index".println()
+            "NEW LINE: $index".println()
             if (previousLine != null) {
                 // Calculate the previous line
                 for (star in previousLine!!.symbols) {
                     if (topNumbers != null) {
-                        adjacentToTop = topNumbers!!
+                        adjacentToTop = topNumbers
                             .map { Pair(star.range.isAdjacent(it.range), it.value) }
                     }
                     val adjacentToPrevious = previousLine!!
                         .numbers
                         .map { Pair(star.range.isAdjacent(it.range), it.value) }
-
                     val adjacentCurrent = numbersFound
                         .map { Pair(star.range.isAdjacent(it.range), it.value) }
-
-                    val adjacentsCount = (adjacentToTop + adjacentToPrevious + adjacentCurrent).filter { it.first }.count()
-                    adjacentToTop.forEach { it.first.println() }.println()
-                    "Adjacent count: $adjacentsCount".println()
-                    if (adjacentsCount == 2) {
-                        sumOfGearRatios += (adjacentToTop + adjacentToPrevious + adjacentCurrent).filter { it.first }.multiplicationOf { it.second.toInt() }
+                    val adjacentValues = (adjacentToTop + adjacentToPrevious + adjacentCurrent).filter { it.first }
+                    if (adjacentValues.count() == 2) {
+                        sumOfGearRatios += adjacentValues.multiplicationOf { it.second.toInt() }
                     }
                 }
                 // Calculate the last line
