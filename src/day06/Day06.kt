287663685bb8b699769bd218a1cf61030a19181f
Maxime Bentin <maxime.bentin@gmail.com>
2023-12-08 20:50:42 +0100

    Day 6 part 1

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -1,11 +1,40 @@
 package day06
 
+import multiplicationOf
 import println
 import readInput
 
+class Race(val time: Int, val distance: Int) {
+    fun winnings(): List<Int> {
+        var list = mutableListOf<Int>()
+        for (i in 1..time) {
+            val traveled = (time - i) * i
+            if (traveled > distance) {
+                list.add(traveled)
+            }
+            "i:$i time:$time traveled:$traveled distance:$distance Added:${(traveled > distance)}".println()
+        }
+        return list
+    }
+}
 fun main() {
+    val timeR = """(\d+)""".toRegex()
     fun part1(input: List<String>): Int {
-        return input.size
+        var time: Sequence<Int> = emptySequence()
+        var distance: Sequence<Int> = emptySequence()
+        for ((index, string) in input.withIndex()) {
+            if (index == 0) {
+                time = timeR.findAll(string).map { it.value.toInt() }
+            } else {
+                distance = timeR.findAll(string).map { it.value.toInt() }
+            }
+        }
+        val races = time.zip(distance).map { Race(it.first, it.second) }
+
+        return races
+            .map { it.winnings().count() }
+            .onEach(::println)
+            .multiplicationOf { it }
     }
 
     fun part2(input: List<String>): Int {
@@ -14,9 +43,11 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06/Day06_test")
-    check(part1(testInput) == 1)
+    // check(part1(testInput) == 1)
 
     val input = readInput("Day06/Day06")
-    part1(input).println()
-    part2(input).println()
+    // part1(testInput).println()
+    //part2(input).println()
+     part1(input).println()
+   // part2(input).println()
 }
