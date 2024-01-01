e95e850d12b6abbc7b761f39e0ad004ed51c42ce
Maxime Bentin <maxime.bentin@gmail.com>
2024-01-01 20:33:07 +0100

    day 13 part 2

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -4,7 +4,6 @@ import println
 import readInput
 import kotlin.math.absoluteValue
 
-
 typealias Pattern = List<String>
 
 fun Pattern.transposed(): Pattern {
@@ -14,7 +13,7 @@ fun Pattern.transposed(): Pattern {
         ""
     }
 
-    for ((index, line) in this.withIndex()) {
+    for (line in this) {
         for (i in 0 ..< size) {
             val sb = StringBuilder(transposed[i])
             sb.append(line[i])
@@ -23,7 +22,6 @@ fun Pattern.transposed(): Pattern {
     }
     return transposed.toList()
 }
-
 fun Pattern.description(): String {
     var sb = StringBuilder("")
     for (line in this) {
@@ -33,20 +31,6 @@ fun Pattern.description(): String {
     }
     return sb.toString()
 }
-
-fun Pattern.shouldBeTransposed(): Boolean {
-    val values = this.withIndex()
-        .groupBy({ it.value }, {it.index})
-        .filterValues { it.count() > 1 }
-
-    val valuesTransposed = this.transposed().withIndex()
-        .groupBy({ it.value }, {it.index})
-        .filterValues { it.count() > 1 }
-    values.count().println()
-    valuesTransposed.count().println()
-    return values.count() <= valuesTransposed.count()
-}
-
 fun Pattern.analyseFrom(index: Int): Boolean {
     var up = index + 1
     var currentState = false
@@ -63,39 +47,22 @@ fun Pattern.analyseFrom(index: Int): Boolean {
     return currentState
 }
 fun Pattern.mirrorIndex(): Int {
-  //  fun Pattern.middle(): Int? {
-  //      var previous = ""
-//
-  //      for ((index, line) in this.withIndex()) {
-  //          if (line == previous) {
-  //              return index - 1
-  //          }
-  //          previous = line
-  //      }
-  //      return null
-  //  }
-
- //   val middle = middle() ?: return 0
-
-  //  middle.let {
-  //      var up = it + 1
-  //      for (i in it downTo 0) {
-  //          if (up >= this.size ) {
-  //              break
-  //          }
-  //          if (this[i] != this[up]) {
-  //              return 0
-  //          }
-  //          up++
-  //      }
-  //  }
-
     val res = indices.filter { this.analyseFrom(it) }
     if (res.isEmpty()) return 0
-    indices.filter { this.analyseFrom(it) }.println()
+    // indices.filter { this.analyseFrom(it) }.println()
     val max = res.max()
     "MAX: $max".println()
-    return max + 1 // middle + 1
+    return max + 1
+}
+fun Pattern.mirrorIndex(filter: Int): Int {
+    val res = indices
+        .filter { this.analyseFrom(it) }
+        .filter { it != filter - 1 }
+    if (res.isEmpty()) return 0
+    // indices.filter { this.analyseFrom(it) }.println()
+    val max = res.first()
+    "MAX: $max".println()
+    return max + 1
 }
 
 fun Pattern.notesSummary(): Int {
@@ -105,6 +72,42 @@ fun Pattern.notesSummary(): Int {
     } else {
         this.transposed().mirrorIndex()
     }
+}
+fun Pattern.notesSummaryWithCorrection(): Int {
+    val originalMirror = mirrorIndex()
+    val originalMirrorTransposed = if (originalMirror == 0) transposed().mirrorIndex() else 0
+    for ((index, line) in this.withIndex()) {
+        for (i in line.indices) {
+            val char = this[index][i]
+            val sb = StringBuilder(line)
+            val copy = this.toMutableList()
+            if (char == '.') {
+                sb.setCharAt(i, '#')
+            } else if (char == '#') {
+                sb.setCharAt(i, '.')
+            }
+
+            copy[index] = sb.toString()
+            val mirrorIndex = copy.mirrorIndex(filter = originalMirror)
+            "Changed line: ${sb} : index: $mirrorIndex".println()
+            if (mirrorIndex > 0 && mirrorIndex != originalMirror) {
+                "FOUND Index: normal".println()
+                copy.description().println()
+                return 100 * mirrorIndex
+            } else {
+                val transposed = copy.transposed()
+                val transposedIndex = transposed.mirrorIndex(filter = originalMirrorTransposed)
+                "transposedIndex: $transposedIndex".println()
+                if (transposedIndex > 0 && transposedIndex != originalMirrorTransposed) {
+                    "FOUND Index: transposed".println()
+                    transposed.description().println()
+                    return transposedIndex
+                }
+            }
+        }
+    }
+    "NO RETURN. Original: $originalMirror, transposed: $originalMirrorTransposed".println()
+    return 0
 }
 
 fun main() {
@@ -135,7 +138,28 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var startIndex = 0
+        val patterns = mutableListOf<Pattern>()
+
+        for ((index, string) in input.withIndex()) {
+            if (string.isEmpty()) {
+                // analyze previous pattern
+                val pattern: Pattern = input.slice(startIndex..< index)
+                patterns.add(pattern)
+                startIndex = index + 1
+            }
+        }
+
+        var sum = 0
+        for (pattern in patterns) {
+            pattern.description().println()
+            pattern.transposed().description().println()
+            val note = pattern.notesSummaryWithCorrection()
+            note.println()
+            sum += note
+        }
+
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
@@ -144,6 +168,7 @@ fun main() {
 
     val input = readInput("Day13/Day13")
    //  part1(testInput).println()
-    part1(input).println() // 31265
-    // part2(input).println()
+  //  part1(input).println() // 31265
+//    part2(testInput).println()
+     part2(input).println() // 34125 < // 30255 < // 33980 <
 }
