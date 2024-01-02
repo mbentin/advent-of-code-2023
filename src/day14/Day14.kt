787f361f5d59ebc6959b5790c2949b8263034e0b
Maxime Bentin <maxime.bentin@gmail.com>
2024-01-02 21:10:56 +0100

    Day 14 part 2

--- src/day14/Day14.kt
+++ src/day14/Day14.kt
@@ -26,6 +26,16 @@ enum class Rock {
 
 typealias Board = Array<Array<Rock?>>
 
+fun Board.description(): String {
+    val stringBuilder = StringBuilder()
+    for ((i, line) in this.withIndex()) {
+        for ((j, char) in line.withIndex()) {
+            stringBuilder.append(if (this[j][i] != null) this[j][i]?.representation() else '.')
+        }
+        stringBuilder.append("\n")
+    }
+    return stringBuilder.toString()
+}
 fun Board.display() {
     for ((i, line) in this.withIndex()) {
         for ((j, char) in line.withIndex()) {
@@ -50,6 +60,8 @@ fun Board.weight(): Int {
 }
 class Platform(input: List<String>) {
     val dish: Board
+    val height: Int
+    val width: Int
     init {
         val length = input.first().count()
         dish = Array(length) { arrayOfNulls(input.size) }
@@ -58,10 +70,40 @@ class Platform(input: List<String>) {
                 dish[j][i] = Rock.getRockBySymbol(char)
             }
         }
+        height = length
+        width = input.size
+    }
+
+    fun cycle(): Board {
+        tiltedBoard()
+        westTiltedBoard()
+        southTiltedBoard()
+        eastTiltedBoard()
+        return dish
+    }
+
+    fun runCycles(): Int {
+        val boards = mutableMapOf<String, Int>()
+        val total = 1_000_000_000
+
+        repeat(total) { iteration ->
+            val board = this.cycle()
+            val description = board.description()
+
+            if (description in boards) {
+                val index = iteration - boards.getValue(description)
+                val remainingCycles = (total - iteration) % index
+                repeat(remainingCycles - 1) { this.cycle() }
+                return dish.weight()
+            }
+
+            boards.put(description, iteration)
+        }
+        return dish.weight()
     }
 
     fun tiltedBoard(): Board {
-        val copy = dish.copyOf()
+        val copy = dish // .copyOf()
         for ((i, line) in dish.withIndex()) {
             var shift = 0
             for ((j, rock) in line.withIndex()) {
@@ -69,18 +111,66 @@ class Platform(input: List<String>) {
                     null -> shift--
                     Rock.Rounded -> {
                         copy[i][j + shift] = rock
-                        if (shift < 0)  {
-                            copy[i][j] = null
-                            // shift++
-                        }
+                        if (shift < 0) copy[i][j] = null
                     }
-                    Rock.Squared -> {
-                        shift = 0
-                    }
+                    Rock.Squared -> shift = 0
                 }
             }
         }
-
+        return copy
+    }
+    fun westTiltedBoard(): Board {
+        val copy = dish // .copyOf()
+        for (i in 0 ..< width) {
+            var shift = 0
+            for (j in 0 ..< height) {
+                val rock = copy[j][i]
+                when(rock) {
+                    null -> shift--
+                    Rock.Rounded -> {
+                        copy[j + shift][i] = rock
+                        if (shift < 0) copy[j][i] = null
+                    }
+                    Rock.Squared -> shift = 0
+                }
+            }
+        }
+        return copy
+    }
+    fun eastTiltedBoard(): Board {
+        val copy = dish // .copyOf()
+        for (i in width - 1 downTo 0) {
+            var shift = 0
+            for (j in height - 1 downTo 0) {
+                val rock = copy[j][i]
+                when(rock) {
+                    null -> shift++
+                    Rock.Rounded -> {
+                        copy[j + shift][i] = rock
+                        if (shift > 0) copy[j][i] = null
+                    }
+                    Rock.Squared -> shift = 0
+                }
+            }
+        }
+        return copy
+    }
+    fun southTiltedBoard(): Board {
+        val copy = dish //.copyOf()
+        for (i in height - 1 downTo 0) {
+            var shift = 0
+            for (j in width - 1 downTo 0) {
+                val rock = copy[i][j]
+                when(rock) {
+                    null -> shift++
+                    Rock.Rounded -> {
+                        copy[i][j + shift] = rock
+                        if (shift > 0) copy[i][j] = null
+                    }
+                    Rock.Squared -> shift = 0
+                }
+            }
+        }
         return copy
     }
 
@@ -95,16 +185,16 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val platform = Platform(input)
+        platform.dish.display()
+        println()
+        return platform.runCycles()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day14/Day14_test")
-   // check(part1(testInput) == 1)
-
     val input = readInput("Day14/Day14")
  //   part1(testInput).println()
-    part1(input).println()
- //   part2(input).println()
- //   part2(input).println()
+ //    part1(input).println()
+  //   part2(testInput).println()
+     part2(input).println() // 94622 > // 94291 > // 94037 > // 92346 // 93742 OK
 }
