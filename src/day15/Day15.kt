94d7d78528deb5a5d215b172a4d99340e4663aa9
Maxime Bentin <maxime.bentin@gmail.com>
2024-01-02 22:24:12 +0100

    Day 15 part 2

--- src/day15/Day15.kt
+++ src/day15/Day15.kt
@@ -22,7 +22,32 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val steps: List<Step> = input.first().split(",")
+        val boxes = List<MutableMap<String, Int>>(256) { mutableMapOf() }
+        for (step in steps) {
+            val split = step.split("=")
+            if (split.count() > 1) {
+                val box = split[0].hash()
+                val label = split[0]
+                val focalLength = split[1].toInt()
+                if (boxes[box].contains(label)) {
+                    boxes[box].replace(label, focalLength)
+                } else {
+                    boxes[box].put(label, focalLength)
+                }
+            } else {
+                val label = step.dropLast(1)
+                val box = label.hash()
+                boxes[box].remove(label)
+            }
+        }
+        var sum = 0
+        for ((boxIndex, box) in boxes.withIndex()) {
+            for ((placeInBox, lens) in box.toList().withIndex()) {
+                sum += (boxIndex + 1) * (placeInBox + 1) * lens.second
+            }
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
@@ -31,7 +56,7 @@ fun main() {
 
     val input = readInput("Day15/Day15")
    // part1(testInput).println()
-    part1(input).println()
-   // part2(input).println()
-   // part2(input).println()
+   // part1(input).println()
+   // part2(testInput).println()
+    part2(input).println()
 }
